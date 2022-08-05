23fb8872e64bf56469b0a6bd0ae6a179861c568b
Carolina Olguin <carolinaolguin1984@yahoo.com.ar>
2022-08-05 02:36:50 -0300

    componente de lista de items

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import {Task} from '../Task'
 import { TASKS } from '../mock-tasks'
+
 /* a el le funciono ../../y demas aca no lo toma asi*/ 
 @Component({
   selector: 'app-tasks',
