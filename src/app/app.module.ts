05d8bccf50c20a23be1d4b6bef052313a9fa1932
Carolina Olguin <carolinaolguin1984@yahoo.com.ar>
2022-08-13 00:26:07 -0300

    tarea realizada y apta para modificaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,11 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+
 const appRoutes:Routes = [
   {path:'' , component:TasksComponent},
-  {path:'about' , component:AboutComponent}
-
+  {path:'about' , component:AboutComponent},
+  
 ]
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ const appRoutes:Routes = [
     AddTaskComponent,
     AboutComponent,
     FooterComponent,
+   
 
   ],
   imports: [
